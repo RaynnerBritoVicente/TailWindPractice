3956a196a505780d8df6da25b5dad9e66bdb4490
RaynnerBritoVicente <raynnergames.rbv@outlook.com>
2023-01-30 11:49:51 -0300

    Patch 1.0,1 / Mobile Adapted

--- QrCode/QrCode/src/App.jsx
+++ QrCode/QrCode/src/App.jsx
@@ -4,14 +4,14 @@ import './App.css'
 function App() {
 
   return (
-    <div className="bg-[#d6e2f0] h-screen w-screen flex flex-col items-center justify-around">
-      <div className='bg-white w-[25%] h-[35rem] rounded-3xl flex flex-col  items-center'>
+    <div className="bg-grayBlue-0 h-screen w-screen flex flex-col items-center justify-around">
+      <div className='bg-white md:w-[25rem] md:h-[35rem] rounded-3xl flex flex-col items-center shadow-xl xs:w-[20rem] xs:h-[31rem]'>
        <div className='flex flex-col items-center text-center'>
-          <img className='border h-[350px] w-[350px] rounded-3xl my-5' src={QR} alt="Qr-Code"/>
-          <strong className='font-outfit font-black w-[16rem] text-[#1f3251] antialiased mb-3'>
+          <img className='border md:h-[22rem] md:w-[22rem] rounded-3xl my-5 xs:h-[18rem] xs:w-[18rem]' src={QR} alt="Qr-Code"/>
+          <strong className='font-outfit font-black md:w-[18rem] text-darkBlue-0 antialiased md:text-xl mb-3 xs:text-lg xs:w-[16rem]'>
             Improve your front-end skills by building projects
           </strong>
-          <p id='corpo' className='font-outfit text-[#7b879d] w-[16rem] antialiased'>
+          <p className='font-outfit text-cGray-0 md:w-[18rem] antialiased xs:text-md xs:w-[18rem]'>
             Scan the QR code to visit Frontend Mentor and take you coding skills to the next level
           </p>
         </div>
--- QrCode/QrCode/tailwind.config.cjs
+++ QrCode/QrCode/tailwind.config.cjs
@@ -11,7 +11,12 @@ module.exports = {
         dosis:['Dosis', 'sans-serif'],
       },
       colors:{
-        
+        grayBlue:['#d6e2f0'],
+        cGray:['#7b879d'],
+        darkBlue:['#1f3251'],
+      },
+      screens:{
+        xs:'375px',
       }
     },
   },
